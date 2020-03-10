4e62cae023d98d780e1adf352bcbe17b2f879f87
Alladoum Ndouba Espérance <alladoumndoubaesperance@gmail.com>
2020-03-11 00:36:31 +0100

    Page de l'authentification implementée

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet,Image,TextInput,Dimensions, TouchableOpacity,ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet,TextInput,Dimensions, TouchableOpacity,ActivityIndicator,Picker,ScrollView } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Icons from '@expo/vector-icons/Ionicons';
-import Color from '../api/color'
+import {Avatar,Image} from 'react-native-elements';
+import NetInfo from '@react-native-community/netinfo';
+import * as ImagePicker from 'expo-image-picker';
+import Useful from '../api/useful';
+import Color from '../api/color';
 const {width} = Dimensions.get('window');
 
 export default class Signup extends Component {
@@ -11,8 +15,14 @@ export default class Signup extends Component {
     this.state = {
         identifiant:'',
         password:'',
+        all:'',
+        sexe:'masculin',
+        tel:'',
+        email:'',
+        photo:'',
         isLoading:false,
         showPass:false,
+        loading:false,
         messageErreur:''
     };
   }
@@ -34,6 +44,34 @@ export default class Signup extends Component {
         isLoading:true
     })
   }
+  uploadImage = async () => {
+    let isConnected = await NetInfo.isConnected.fetch();
+    let continued = true;
+    let pickerResult;
+    if(isConnected) {
+        if(this.state.photo != '') return;
+        this.setState({loading:true});
+        let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+        try {
+            if(permissionResult.granted  === false) {
+                Useful.displayAlert("Erreur","Permission d'accès à la camera refusée");
+                return;
+            }
+            pickerResult = await ImagePicker.launchImageLibraryAsync();
+            if (pickerResult.cancelled === true) return;
+            this.setState({photo:pickerResult.uri,loading:false,messageErreur:''});
+        } catch (error) {
+            Useful.displayAlert('Erreur','Rechoisissez une photo');
+            continued = false;
+        } finally {
+            if(continued) {
+                Useful.showToast('Votre photo a été bien téléchargée');
+            }
+        }
+    }else {
+        Useful.displayAlert("Information","Vous n'êtes pas connecté sur internet")
+        }
+    }
   renderButtonLogin = () => {
     if (this.state.isLoading) {
         return (
@@ -50,83 +88,143 @@ export default class Signup extends Component {
   }
 
   render() {
-    const {showPass,identifiant,password,messageErreur} = this.state
+    const {showPass,identifiant,password,messageErreur,all,tel,email,photo,sexe} = this.state
     return (
       <SafeAreaView style={styles.container}>
-        <View style={styles.titleContainer}>
-            <Text style={styles.title}>Connexion</Text>
-        </View>
-        <View style={styles.inputContainer}>
-            <Icons 
-                name='ios-person'
-                size={28}
-                color = {Color.primary}
-                style = {styles.inputIcon}
-            />
-            <TextInput
-                style={styles.input}
-                onChangeText = {text => this.onChangeText('identifiant',text)}
-                value={identifiant}
-                placeholder="Identifiant"
-                placeholderTextColor='black'
-                underlineColorAndroid='transparent'
-                onSubmitEditing={() => this.secondTextInput.focus()}
-                autoCapitalize='none'
-                returnKeyType='next'
-                blurOnSubmit={false}
-            />
-        </View>
-        <View style={styles.inputContainer} >
-            <Icons 
-                name='ios-lock'
-                size={28}
-                color = {Color.primary}
-                style = {styles.inputIcon}
-            />
-            <TextInput
-                style={styles.input}
-                onChangeText ={text => this.onChangeText('password',text)}
-                value={password}
-                placeholder="Mot de passe"
-                secureTextEntry={showPass}
-                placeholderTextColor='black'
-                underlineColorAndroid='transparent'
-                autoCapitalize='none'
-                returnKeyType='send'
-                blurOnSubmit={false}
-                ref={input => this.secondTextInput = input}
-                onSubmitEditing={this.login}
-            />
-            <TouchableOpacity style={styles.btnEye} onPress={this.showPassword}>
-                <Icons 
-                    name={showPass?'ios-eye':'ios-eye-off'}
-                    size={28}
-                    color={Color.primary}
+        <ScrollView>
+
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>Inscription</Text>
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText = {text => this.onChangeText('all',text)}
+                    value={all}
+                    placeholder="Nom complet"
+                    placeholderTextColor='black'
+                    underlineColorAndroid='transparent'
+                    onSubmitEditing={() => this.identifiant.focus()}
+                    autoCapitalize='none'
+                    returnKeyType='next'
+                    blurOnSubmit={false}
                 />
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText = {text => this.onChangeText('identifiant',text)}
+                    value={identifiant}
+                    placeholder="Identifiant"
+                    placeholderTextColor='black'
+                    underlineColorAndroid='transparent'
+                    onSubmitEditing={() => this.password.focus()}
+                    ref={input => this.identifiant = input}
+                    autoCapitalize='none'
+                    returnKeyType='next'
+                    blurOnSubmit={false}
+                />
+            </View>
+            <View style={styles.inputContainer} >
+                <TextInput
+                    style={styles.input}
+                    onChangeText ={text => this.onChangeText('password',text)}
+                    value={password}
+                    placeholder="Mot de passe"
+                    secureTextEntry={showPass}
+                    placeholderTextColor='black'
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
+                    returnKeyType='send'
+                    blurOnSubmit={false}
+                    ref={input => this.password = input}
+                    onSubmitEditing={() => this.tel.focus()}
+                />
+                <TouchableOpacity style={styles.btnEye} onPress={this.showPassword}>
+                    <Icons 
+                        name={showPass?'ios-eye':'ios-eye-off'}
+                        size={28}
+                        color={Color.primary}
+                    />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText = {text => this.onChangeText('tel',text)}
+                    value={tel}
+                    placeholder="Téléphone"
+                    placeholderTextColor='black'
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
+                    returnKeyType='next'
+                    blurOnSubmit={false}
+                    keyboardType='number-pad'
+                    ref={input => this.tel = input}
+                    onSubmitEditing={() => this.email.focus()}
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText = {text => this.onChangeText('email',text)}
+                    value={email}
+                    placeholder="Adresse Email"
+                    placeholderTextColor='black'
+                    underlineColorAndroid='transparent'
+                    autoCapitalize='none'
+                    returnKeyType='next'
+                    blurOnSubmit={false}
+                    keyboardType='email-address'
+                    ref={input => this.email = input}
+                />
+            </View>
+            <View style={styles.typeContainer}>
+                <Text style={styles.defaultText}>Sexe:</Text>
+                <Picker selectedValue={sexe} onValueChange={sexe => this.setState({sexe})}>
+                    <Picker.Item label="Masculin" value="masculin" />
+                    <Picker.Item label="Feminin" value="feminin" />
+                </Picker>
+            </View>
+            <View style={styles.typeContainer}>
+                <View style={{flexDirection:'row',paddingLeft:5,paddingTop:5,alignItems:'center'}}>
+                    <Avatar
+                        size={60}
+                        rounded
+                        icon={{name: 'camera', type: 'font-awesome'}}
+                        onPress={() => this.uploadImage()}
+                        activeOpacity={0.7}
+                        />
+                    {
+                        photo != null && (
+                            <View style={styles.containerImage}>
+                                <Image
+                                    style={styles.thumbnail}
+                                    source={{uri:photo}}
+                                    PlaceholderContent={<ActivityIndicator/>}
+                                />
+                            </View>
+                        )
+                    }  
+                </View>
+            </View>
+            {
+                messageErreur != "" && (
+                        <View style={{alignItems:'center'}}>
+                            <Text style={{color:'red'}}>{messageErreur}</Text>
+                        </View>
+                        )
+                    }
+            <TouchableOpacity style={styles.btnLogin} onPress={this.login}>
+                {this.renderButtonLogin()}
             </TouchableOpacity>
-        </View>
-        {
-            messageErreur != "" && (
-                    <View style={{alignItems:'center'}}>
-                        <Text style={{color:'red'}}>{messageErreur}</Text>
-                    </View>
-                    )
-                }
-        <TouchableOpacity style={styles.btnLogin} onPress={this.login}>
-            {this.renderButtonLogin()}
-        </TouchableOpacity>
-        <View>
-            <Text style={{color:'black',fontWeight:'bold'}}>OU</Text>
-        </View>
-        <TouchableOpacity style={styles.btnGle} onPress={this.login}>
-            <Text style={styles.btnText}>Compte google</Text>
-        </TouchableOpacity>
-        <View style={styles.btnSignup}>
-            <Text>Pas de compte?</Text>
-            <TouchableOpacity>
-                <Text style={{color:Color.primary,fontWeight:'bold',paddingLeft:5}}>Inscription</Text>
-            </TouchableOpacity>
-        </View>
+            <View style={styles.btnSignup}>
+                <Text>Déjà un compte?</Text>
+                <TouchableOpacity>
+                    <Text style={{color:Color.primary,fontWeight:'bold',paddingLeft:5}}>Connexion</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
       </SafeAreaView>
     );
   }
@@ -135,11 +233,10 @@ export default class Signup extends Component {
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        alignItems:'center',
         backgroundColor: '#fff',
     },
     titleContainer: {
-        margin:40,
+        margin:20,
         justifyContent: 'center',
     },
     title: {
@@ -156,22 +253,36 @@ const styles = StyleSheet.create({
         height:45,
         borderRadius:25,
         fontSize:16,
-        paddingLeft:45,
+        paddingLeft:10,
         color:'black',
         marginHorizontal:25,
         borderWidth: 1,
         borderColor: Color.primary,
         marginBottom: 10,
     },
-    inputIcon: {
-        position: 'absolute',
-        top:8,
-        left:40
-    },
     btnEye: {
         position: 'absolute',
         top:8,
         right:37
+    },
+    typeContainer: {
+        marginLeft:20
+    },
+    defaultText :{
+        color:'black',
+        fontWeight:'bold',
+        paddingLeft:5,
+        textAlign:'left'
+    },
+    containerImage: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    thumbnail:{
+        width:120,
+        height:120,
+        resizeMode:'contain'
     },
     btnLogin: {
         width:width - 100,
@@ -180,14 +291,7 @@ const styles = StyleSheet.create({
         marginVertical:10,
         backgroundColor:Color.primary,
         justifyContent: 'center',
-    },
-    btnGle: {
-        width:width - 100,
-        height:45,
-        borderRadius:25,
-        marginTop:10,
-        backgroundColor:Color.secondary,
-        justifyContent: 'center',
+        marginLeft: 60,
     },
     btnText: {
         color:'#fff',
@@ -199,6 +303,6 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         justifyContent: 'center',
         alignItems: 'flex-end',
-        margin: 10,
+        marginTop:3,
     }
 })
